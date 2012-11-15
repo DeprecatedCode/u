b89f6dc1dde1e73971cd3b18d494b371e5870c84
Nate Ferrero <nateferrero@gmail.com>
2012-11-15 10:39:51 +0000

    Sparse token-traversal, ready for input

--- implementations/php/lib/sparse.php
+++ implementations/php/lib/sparse.php
@@ -42,26 +42,128 @@ class Sparse {
      * Apply the grammar to a string, generating an array of tokens
      */
     public function apply($str, $context='root') {
-        $pointer = 0;
-        $line = 0;
-        $col = 0;
-        $tokens = array();
-        $stack = &$tokens;
-        $length = strlen($str);
+        $doc = new SparseDocument($this->grammar, $str, $context);
+        return $doc->tokenize();
+    }
+}
 
-        while($pointer < $length) {
+/**
+ * Sparse document
+ */
+class SparseDocument {
 
-            if(!isset($this->grammar[$context])) {
-                throw new ParseException($line, $col, "invalid-context", $context);
+    private $pointer = 0;
+    private $line = 0;
+    private $col = 0;
+    private $length = 0;
+    private $str;
+    private $tokens;
+    private $grammar;
+    private $tree;
+    private $tip;
+    private $stack = array();
+    private $context;
+
+    public function __construct(&$grammar, &$str, $context) {
+        $this->grammar = $grammar;
+        $this->str = $str;
+        $this->length = strlen($str);
+        $this->tokens = $grammar['&tokens'];
+        $this->context = $context;
+        $tmp = $this->node(null);;
+        $this->tree = &$tmp;
+        $this->tip = &$tmp;
+    }
+
+    /**
+     * Create a node
+     */
+    public function node($match) {
+        if(!isset($this->grammar[$this->context])) {
+           throw new ParseException($this->line, $this->col,
+               "invalid-context", $this->context);
+        }
+        return array(
+            'token' => $this->context,
+            'match' => $match,
+            'content' => null
+        );
+    }
+
+    /**
+     * Create a child of the tip
+     */
+    public function &child($match) {
+        $tmp = $this->node($match);
+        if(!isset($this->tip['children'])) {
+            $this->tip['children'] = array();
+        }
+        $this->tip['children'][] = &$tmp;
+        return $tmp;
+    }
+
+    /**
+     * Drop into a child token
+     */
+    public function descend($match) {
+        $this->stack[] = &$this->tip;
+        $tmp = &$this->child($match);
+        unset($this->tip);  # Break ref
+        $this->tip = &$tmp;
+    }
+
+    /**
+     * Pop up out of a child token
+     */
+    public function ascend($match) {
+        $this->tip['exit'] = $match;
+        $tmp = &$this->stack[count($this->stack) - 1];
+        array_pop($this->stack);
+        unset($this->tip);  # Break ref
+        $this->tip = &$tmp;
+    }
+
+    /**
+     * Tokenize
+     */
+    public function &tokenize() {
+
+        /*
+        TODO Add as proper test
+
+        $this->descend('A');
+        $this->descend('B');
+        $this->descend('C');
+        $this->child('@d');
+        $this->ascend('--c--');
+        $this->child('@c');
+        $this->ascend('--b--');
+        $this->child('@b1');
+        $this->child('@b2');
+        $this->child('@b3');
+        $this->ascend('--a--');
+
+        echo "After Tree: "; print_r($this->tree);
+        */
+
+        while($this->pointer < $this->length) {
+            /**
+             * Current context instructions
+             */
+            $scope = $this->grammer[$context];
+
+            /**
+             * Handle children first
+             */
+            if(isset($scope['&children'])) {
+                foreach($scope['&children'] as $def) {
+                   echo $def . '   ';
+                }
             }
-
-            foreach($this->grammar[$context] as $token => $def) {
-
-            }
-            
-            throw new ParseException($line, $col, "no-match", substr($str, $pointer));
+            throw new ParseException($this->line, $this->col, "no-match",
+                substr($this->str, $this->pointer));
         }
 
-        return $tokens;
+        return $this->tree;;
     }
 }
